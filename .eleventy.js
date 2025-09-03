472e6658e16fe359c569bc5edf9b4ed462f4fefb
Michiko <inouem888@gmail.com>
2025-09-03 17:06:13 +0200

    Add blog post layout and update Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,13 @@ module.exports = (cfg) => {
 
   cfg.addShortcode("year", () => new Date().getFullYear());
 
+  // Add blog 
+  cfg.addCollection("blog_ja", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/ja/blog/*.md").sort((a, b) => {
+      return b.date - a.date; 
+    });
+  });
+
   return {
     dir: { input: "src", includes: "_includes", data: "_data", output: "_site" },
     htmlTemplateEngine: "njk",
